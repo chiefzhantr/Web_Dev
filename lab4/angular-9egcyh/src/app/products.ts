6f1f1e74cb97fdeb0b61d14f8213966443d5900a
Svanov Zhantore <@n.akylbaev>
2023-02-22 00:53:12 +0600

    tutorial finished

---
+++ lab4/angular-9egcyh/src/app/products.ts
@@ -0,0 +1,38 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  // image: string;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    // image: 'assets/phone.phg',
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    // image: 'assets/mouse.phg',
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    // image: 'assets/backpack.phg',
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
